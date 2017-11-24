d8164dbe429f2daa54b8b248c07a08dcc5b5beee
Syny <s.raouf@student.isartdigital.com>
2017-11-23 22:10:03 -0500

    Setup

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <GL\glew.h>
+#include <GL\freeglut.h>
+
+void render();
+
+int main(int argc, char* argv[]) 
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(640, 480);
+	glutCreateWindow("Glut Window");
+
+	glutDisplayFunc(render);
+
+	glutMainLoop();
+	return 0;
+}
+
+void render()
+{
+	
+}
