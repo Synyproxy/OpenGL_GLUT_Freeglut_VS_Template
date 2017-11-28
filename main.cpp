14110a3995a172ab61dd43755ed3e1b8e2447aec
Syny Proxy <s.raouf@student.isartdigital.com>
2017-11-28 16:45:52 -0500

    Updated main.cpp centered window - Displayfunc

--- main.cpp
+++ main.cpp
@@ -1,22 +1,47 @@
-#include <GL\freeglut.h>
+#include <GL/freeglut.h>
 
-void render();
+void InitGL();
+void Reshape(int width, int height);
+void Display();
 
-int main(int argc, char* argv[]) 
+int main(int argc, char* argv[])
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(640, 480);
-	glutCreateWindow("Glut Window");
+	int windowWidth, windowHeight;
+	
+	windowWidth = 1280;
+	windowHeight = 720;
 
-	glutDisplayFunc(render);
+	//Glut Init
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowSize(windowWidth, windowHeight);
+	glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH) - windowWidth) / 2, (glutGet(GLUT_SCREEN_HEIGHT) - windowHeight) / 2);
+	glutCreateWindow("OpenGL FreeGlut Window");
+
+	//Callbacks
+	glutReshapeFunc(Reshape);
+	glutDisplayFunc(Display);
+	InitGL();
 
 	glutMainLoop();
+
 	return 0;
 }
 
-void render()
+void InitGL()
 {
+	glClearColor(0, 0, 0, 1);
+	glEnable(GL_DEPTH_TEST);
+}
+
+void Reshape(int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void Display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
+	glFlush();
 }
